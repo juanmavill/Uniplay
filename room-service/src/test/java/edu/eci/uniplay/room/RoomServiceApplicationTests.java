d6bac0a2d694a104afec7d2a46b9cebc64a89d87
juanmavill <juan.vmedina@mail.escuelaing.edu.co>
2026-07-07 19:13:08 -0500

    build(room-service): inicializar proyecto spring boot

---
+++ room-service/src/test/java/edu/eci/uniplay/room/RoomServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package edu.eci.uniplay.room;
+
+import org.junit.jupiter.api.Test;
+
+class RoomServiceApplicationTests {
+
+    @Test
+    void applicationClassExists() {
+        new RoomServiceApplication();
+    }
+}
