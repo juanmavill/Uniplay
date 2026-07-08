e3522894c2ff8062248cb8bdf8bf515c4dc3fd55
juanmavill <juan.vmedina@mail.escuelaing.edu.co>
2026-07-07 20:51:50 -0500

    build(realtime-service): inicializar proyecto spring boot

---
+++ realtime-service/src/test/java/edu/eci/uniplay/realtime/RealtimeServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package edu.eci.uniplay.realtime;
+
+import org.junit.jupiter.api.Test;
+
+class RealtimeServiceApplicationTests {
+
+    @Test
+    void applicationClassExists() {
+        new RealtimeServiceApplication();
+    }
+}
